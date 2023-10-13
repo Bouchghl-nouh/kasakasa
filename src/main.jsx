d69eb95282cf568b4a8e9c8240edf4185f8eb817
Zero <bouchghlahmed@gmail.com>
2023-10-13 16:17:05 +0100

    fdsajfdlksadvvv

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import './CSS/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./CSS/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter  basename="/Kasa_Nouh">
+    <BrowserRouter basename="/kasakasa">
       <App />
     </BrowserRouter>
   </React.StrictMode>
